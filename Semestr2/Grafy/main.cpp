0ef28c4817f0d0b6da1ac6f633c18d8f61ea5414
Tomek <tomek.rozycki@student.uj.edu.pl>
2022-04-04 11:59:10 +0200

    koniec zajec

--- Semestr2/Grafy/main.cpp
+++ Semestr2/Grafy/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Vertex
 {
+    // unikalny numer wierzcholka
     int number;
 
 public:
     int weight;
     std::string label;
-    Vertex(int n);
-    int Number() const;
+    Vertex(int n) : number(n){};
+    int Number() const { return number; };
 };
 
 class Edge
@@ -26,82 +28,120 @@ public:
     Edge(Vertex *V0, Vertex *V1);
     Vertex *V0() { return v0; };
     Vertex *V1() { return v1; };
-    Vertex *Mate(Vertex *v);
+
+    // zwraca drugi wierzcholek krawedzi
+    Vertex *Mate(Vertex *v){
+
+    };
 };
 
 class GraphAsMatrix
 {
-    std::vector<Vertex *> vertices;
-    std::vector<std::vector<Edge *>> adjacencyMatrix;
+    std::vector<Vertex *> vertices;                   // vector wierzcholkow grafu
+    std::vector<std::vector<Edge *>> adjacencyMatrix; // macierz sasiedztwa
     bool isDirected;
     int numberOfVertices;
     int numberOfEdges = 0;
 
-    //     class AllVerticesIter : public Iterator<Vertex>
-    //     {
-    //         GraphAsMatrix &owner;
-    //         int current;
+    /*
+            class AllVerticesIter : public Iterator<Vertex>
+            {
+                GraphAsMatrix &owner;
+                int current;
 
-    //     public:
-    //         AllVerticesIter(GraphAsMatrix &owner);
-    //         bool IsDone();
-    //         Vertex &operator*();
-    //         void operator++();
-    //     };
-    //     class AllEdgesIter : public Iterator<Edge>
-    //     {
-    //         GraphAsMatrix &owner;
-    //         int row;
-    //         int col;
+            public:
+                AllVerticesIter(GraphAsMatrix &owner);
+                bool IsDone();
+                Vertex &operator*();
+                void operator++();
+            };
+            class AllEdgesIter : public Iterator<Edge>
+            {
+                GraphAsMatrix &owner;
+                int row;
+                int col;
 
-    //     public:
-    //         void next();
-    //         AllEdgesIter(GraphAsMatrix &owner);
-    //         bool IsDone();
-    //         Edge &operator*();
-    //         void operator++() { next(); }
-    //     };
-    //     class EmanEdgesIter : public Iterator<Edge>
-    //     {
-    //         GraphAsMatrix &owner;
-    //         int row;
-    //         int col;
+            public:
+                void next();
+                AllEdgesIter(GraphAsMatrix &owner);
+                bool IsDone();
+                Edge &operator*();
+                void operator++() { next(); }
+            };
+            class EmanEdgesIter : public Iterator<Edge>
+            {
+                GraphAsMatrix &owner;
+                int row;
+                int col;
 
-    //     public:
-    //         void next();
-    //         EmanEdgesIter(GraphAsMatrix &owner, int v);
-    //         bool IsDone();
-    //         Edge &operator*();
-    //         void operator++() { next(); }
-    //     };
-    //     class InciEdgesIter : public Iterator<Edge>
-    //     {
-    //         GraphAsMatrix &owner;
-    //         int row;
-    //         int col;
+            public:
+                void next();
+                EmanEdgesIter(GraphAsMatrix &owner, int v);
+                bool IsDone();
+                Edge &operator*();
+                void operator++() { next(); }
+            };
+            class InciEdgesIter : public Iterator<Edge>
+            {
+                GraphAsMatrix &owner;
+                int row;
+                int col;
 
-    //     public:
-    //         void next();
-    //         InciEdgesIter(GraphAsMatrix &owner, int v);
-    //         bool IsDone();
-    //         Edge &operator*();
-    //         void operator++() { next(); }
-    //     };
+            public:
+                void next();
+                InciEdgesIter(GraphAsMatrix &owner, int v);
+                bool IsDone();
+                Edge &operator*();
+                void operator++() { next(); }
+            };
+            */
 
-    // public:
-    //     GraphAsMatrix(int n, bool b);
-    //     int NumberOfVertices();
-    //     bool IsDirected();
-    //     int NumberOfEdges();
-    //     bool IsEdge(int u, int v);
-    //     void MakeNull();
-    //     void AddEdge(int u, int v);
-    //     void AddEdge(Edge *edge);
-    //     Edge *SelectEdge(int u, int v);
-    //     Vertex *SelectVertex(int v);
+public:
+    // liczba wierzcholkow, czy skierowany
+    GraphAsMatrix(int n, bool b) : numberOfVertices(n), isDirected(b)
+    {
+        // tworzymy vector wierzcholkow, unikalne numery od 0 do n
+        // tworzymy macierz sasiedztwa ustawiajac ja na null, potem metoda
+        // addedges bedziemy dodawac krawedzie
+        for (int i = 0; i < numberOfVertices; i++)
+        {
+            vertices.push_back();
+        }
 
-    //     Iterator<Vertex> &VerticesIter();
-    //     Iterator<Edge> &EdgesIter();
-    //     Iterator<Edge> &EmanatingEdgesIter(int v);
-    //     Iterator<Edge> &IncidentEdgesIter(int v);
+        //ustawiamy macierz sasiedztwa na zera
+        fill(adjacencyMatrix.begin(), adjacencyMatrix.end(), 0);
+    };
+
+    int NumberOfVertices() { return numberOfVertices; };
+    bool IsDirected() { return isDirected; };
+    int NumberOfEdges() { return numberOfEdges; };
+
+    bool IsEdge(int u, int v);
+    void MakeNull();
+    void AddEdge(int u, int v);
+    void AddEdge(Edge *edge);
+    Edge *SelectEdge(int u, int v){
+
+    };
+
+    // zwraca wskaznik do v-tego wierzcholka grafu
+    Vertex *SelectVertex(int v)
+    {
+        if (v >= 0 && v < numberOfVertices)
+        {
+            return vertices[v];
+        }
+        else
+        {
+            cout << "Blad" << endl;
+            exit;
+        }
+    };
+
+    /*
+            Iterator<Vertex> &VerticesIter();
+            Iterator<Edge> &EdgesIter();
+            Iterator<Edge> &EmanatingEdgesIter(int v);
+            Iterator<Edge> &IncidentEdgesIter(int v);
+    */
 };
